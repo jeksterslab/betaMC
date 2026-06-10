a0cdc6fc2bb07a0be8580be612da61fccf1ecb2b
jeksterslab <jeksterslab@users.noreply.github.com>
2026-06-10 22:53:01 +0000

    Automated build [skip ci].
    Files changed:
    A	.Rbuildignore
    A	.setup/build/betaMC.pdf
    A	.setup/build/betaMC_1.3.4.tar.gz
    A	.setup/data-raw/benchmark.Rds
    A	.setup/latex/bib/quarto.bib
    A	CITATION.cff
    M	DESCRIPTION
    A	LICENSE
    A	LICENSE.md
    A	NAMESPACE
    A	R/betaSandwich-acov-hc-dot.R
    A	R/betaSandwich-acov-sem-inverse-dot.R
    A	R/betaSandwich-cov-hc-dot.R
    A	R/betaSandwich-gamma-hc-dot.R
    A	R/betaSandwich-q-mat-dot.R
    A	R/dataSets-nas1982.R
    A	R/gammaADF-gamma-adf-consistent-dot.R
    A	R/gammaADF-gamma-adf-unbiased-dot.R
    A	R/gammaN-gamma-mvn-dot.R
    A	R/linearAlgebra-d-mat-dot.R
    A	R/linearAlgebra-d-of-mat-dot.R
    A	R/linearAlgebra-pinv-of-d-mat-dot.R
    A	R/linearAlgebra-positive-definite-2-test-dot.R
    A	R/linearAlgebra-positive-definite-test-dot.R
    A	R/linearAlgebra-sym-of-vech-dot.R
    A	R/linearAlgebra-vec-dot.R
    A	R/linearAlgebra-vech-dot.R
    A	R/linearAlgebra-vech-names-dot.R
    A	R/miHelper-ariv-dot.R
    A	R/miHelper-mi-combine-dot.R
    A	R/miHelper-total-adj-dot.R
    A	R/nBootstrap-ci-format-dot.R
    A	R/nBootstrap-pc-ci-dot.R
    A	R/nBootstrap-pc-probs-dot.R
    A	R/processLM-dif-dot.R
    A	R/processLM-process-lm-dot.R
    A	R/randomGaussian-location-dot.R
    A	R/randomGaussian-random-gaussian-chol-dot.R
    A	R/randomGaussian-random-gaussian-eigen-dot.R
    A	R/randomGaussian-random-gaussian-svd-dot.R
    A	R/randomGaussian-random-gaussian-z-dot.R
    A	R/rhoMatrix-rho-of-sigma-dot.R
    A	R/semmcci-theta-hat-star-dot.R
    A	R/strRegression-beta-star-dot.R
    A	R/strRegression-beta-star-of-rho-dot.R
    A	R/strRegression-jacobian-vech-sigma-wrt-theta-dot.R
    A	R/strRegression-moments-index-dot.R
    A	R/strRegression-p-cor-sq-dot.R
    A	R/strRegression-r-sq-bar-dot.R
    A	R/strRegression-r-sq-of-sigma-dot.R
    A	R/strRegression-s-p-cor-dot.R
    A	R/strRegression-sigma-y-sq-dot.R
    A	R/strRegression-sigma-yx-dot.R
    A	R/strRegression-theta-index-dot.R
    A	README.md
    M	data/nas1982.rda
    A	man/BetaMC.Rd
    A	man/DeltaRSqMC.Rd
    A	man/DiffBetaMC.Rd
    A	man/MC.Rd
    A	man/MCMI.Rd
    A	man/PCorMC.Rd
    A	man/RSqMC.Rd
    A	man/SCorMC.Rd
    A	man/betaMC-package.Rd
    A	man/coef.betamc.Rd
    A	man/confint.betamc.Rd
    A	man/nas1982.Rd
    A	man/print.betamc.Rd
    A	man/print.mc.Rd
    A	man/summary.betamc.Rd
    A	man/summary.mc.Rd
    A	man/vcov.betamc.Rd
    M	project.Rproj
    A	vignettes/benchmark.Rmd
    A	vignettes/example-beta-mc.Rmd
    A	vignettes/example-delta-r-sq-mc.Rmd
    A	vignettes/example-diff-beta-mc.Rmd
    A	vignettes/example-mc-mi.Rmd
    A	vignettes/example-p-cor-mc.Rmd
    A	vignettes/example-r-sq-mc.Rmd
    A	vignettes/example-s-cor-mc.Rmd
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-15-1.png
    A	vignettes/session.Rmd
    A	vignettes/vignettes.bib

---
+++ R/linearAlgebra-d-mat-dot.R
@@ -0,0 +1,35 @@
+#' The Duplication Matrix
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @param k Positive integer.
+#'   Dimension of the `k` by `k` matrix.
+#'
+#' @return Returns a matrix.
+#'
+#' @family Symmetric Functions
+#' @keywords linearAlgebra symmetric internal
+#' @noRd
+.DMat <- function(k) {
+  sym <- matrix(
+    0,
+    nrow = k,
+    ncol = k
+  )
+  q <- seq_len(
+    0.5 * k * (k + 1)
+  )
+  sym[lower.tri(sym, diag = TRUE)] <- q
+  sym[upper.tri(sym)] <- t(sym)[upper.tri(sym)]
+  outer(
+    X = .Vec(sym),
+    Y = q,
+    FUN = function(x, y) {
+      ifelse(
+        test = x == y,
+        yes = 1,
+        no = 0
+      )
+    }
+  )
+}
