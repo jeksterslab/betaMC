717979c0596c586a35a0dc382aa865977182676b
jeksterslab <learn.jeksterslab@gmail.com>
2026-06-10 22:39:48 +0000

    Automated build.

--- .setup/scripts/project.R
+++ .setup/scripts/project.R
@@ -34,6 +34,8 @@ pkg_ver <- c()
 
 ver <- c()
 
+pkg_bioconductor <- c()
+
 ignore <- "^vignettes$"
 
 license <- "mit"
--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,9 @@
+# betaMC 1.3.4
+
+## Patch
+
+* Minor documentation edits.
+
 # betaMC 1.3.3
 
 ## Patch
